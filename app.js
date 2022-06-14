66703f5fdede098ae828db8c6413d348fb76190a
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-06-15 01:57:51 +0530

    setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/hello', (request, response) => {
+    response.send('Task Manager App');
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+} )
+
+
